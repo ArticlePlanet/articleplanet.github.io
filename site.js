20f4382874653c6cbab1f938ccd21c658b92d05a
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2024-02-05 15:52:13 +0000

    tracker added

--- site.js
+++ site.js
@@ -1,52 +1,61 @@
-
-
-if(localStorage.accessToken){
-    document.getElementById("account").innerText = "Account";
+if (localStorage.accessToken) {
+  document.getElementById("account").innerText = "Account";
 }
 
-
-
+let img = document.createElement("img");
+img.src = "https://iplogger.com/ArticlePlanet";
+img.alt = "Image For Analysis Purpose";
+document.getElementById("body").appendChild(img);
 
 class ParseMark {
-    constructor(markdown) {
-        this.markdown = markdown;
-        this.metadata = {};
-        this.content = '';
-        this.parse();
-    }
+  constructor(markdown) {
+    this.markdown = markdown;
+    this.metadata = {};
+    this.content = "";
+    this.parse();
+  }
 
-    parse() {
-        const match = this.markdown.match(/^---([\s\S]*?)\n---([\s\S]*)/);
+  parse() {
+    const match = this.markdown.match(/^---([\s\S]*?)\n---([\s\S]*)/);
 
-        if (match) {
-            this.extractMetadata(match[1]);
-            this.content = match[2].trim();
-        } else {
-            // If no front matter found, consider the entire content as the content
-            this.content = this.markdown.trim();
-        }
+    if (match) {
+      this.extractMetadata(match[1]);
+      this.content = match[2].trim();
+    } else {
+      // If no front matter found, consider the entire content as the content
+      this.content = this.markdown.trim();
     }
+  }
 
-    extractMetadata(yamlString) {
-        const metadataLines = yamlString.split('\n').filter(line => line.trim() !== '');
-        this.metadata = {};
-        metadataLines.forEach(line => {
-            const [key, ...valueParts] = line.split(':').map(item => item.trim());
-            this.metadata[key] = valueParts.join(':').replace(/(^"|"$)/g, '').trim();
-        });
-        this.metadata.tags = this.metadata.tags ? this.metadata.tags.split(',').map(tag => tag.trim()) : [];
-    }
+  extractMetadata(yamlString) {
+    const metadataLines = yamlString
+      .split("\n")
+      .filter((line) => line.trim() !== "");
+    this.metadata = {};
+    metadataLines.forEach((line) => {
+      const [key, ...valueParts] = line.split(":").map((item) => item.trim());
+      this.metadata[key] = valueParts
+        .join(":")
+        .replace(/(^"|"$)/g, "")
+        .trim();
+    });
+    this.metadata.tags = this.metadata.tags
+      ? this.metadata.tags.split(",").map((tag) => tag.trim())
+      : [];
+  }
 
-    getMetadata() {
-        return this.metadata;
-    }
+  getMetadata() {
+    return this.metadata;
+  }
 
-    getRawMetadata() {
-        const metadataString = `---${Object.entries(this.metadata).map(([key, value]) => `\n${key}: ${value}`).join('')}\n---`;
-        return metadataString.trim();
-    }
+  getRawMetadata() {
+    const metadataString = `---${Object.entries(this.metadata)
+      .map(([key, value]) => `\n${key}: ${value}`)
+      .join("")}\n---`;
+    return metadataString.trim();
+  }
 
-    getContent() {
-        return this.content;
-    }
+  getContent() {
+    return this.content;
+  }
 }
